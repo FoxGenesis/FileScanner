82cc8747eb046fde592569b6e75477daaa4fa190
Ashley <ashley@foxgenesis.net>
2023-02-04 21:39:38 -0600

    renamed module

--- src/module-info.java
+++ src/module-info.java
@@ -3,11 +3,11 @@
  *
  * @provides net.foxgenesis.watame.plugin.IPlugin
  */
-module pluginTemplate {
+module watamebot.filescanner {
 	requires watamebot;
 	requires net.dv8tion.jda;
 	requires java.base;
-	requires jsr305;
+
 
 	provides net.foxgenesis.watame.plugin.IPlugin with net.foxgenesis.watame.filescanner.FileScannerPlugin;
 }
