a206291d0eaf84978405fc0d0105cf7977133d3f
Ashley <ashley@foxgenesis.net>
2025-09-15 15:44:55 -0500

    Update FileScannerController.java

--- src/main/java/net/foxgenesis/filescanner/web/FileScannerController.java
+++ src/main/java/net/foxgenesis/filescanner/web/FileScannerController.java
@@ -1,6 +1,7 @@
 package net.foxgenesis.filescanner.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,9 @@ import jakarta.validation.Valid;
 import net.dv8tion.jda.api.entities.Guild;
 import net.foxgenesis.filescanner.database.FileScannerConfiguration;
 import net.foxgenesis.filescanner.database.FileScannerConfigurationService;
+import net.foxgenesis.watame.web.WebPanelUtil;
 import net.foxgenesis.watame.web.annotation.PluginMapping;
+import net.foxgenesis.watame.web.security.DiscordOAuth2User;
 
 @PluginMapping(plugin = "filescanner")
 public class FileScannerController {
@@ -20,23 +23,30 @@ public class FileScannerController {
 	@Autowired
 	private FileScannerConfigurationService database;
 
+	@Autowired
+	private WebPanelUtil util;
+
 	@GetMapping
 	public String getView(Model model, @RequestAttribute Guild guild) {
-		model.addAttribute("fileScannerConfiguration", database.getFresh(guild).orElseGet(() -> new FileScannerConfiguration(guild)));
+		model.addAttribute("fileScannerConfiguration",
+				database.getFresh(guild).orElseGet(() -> new FileScannerConfiguration(guild)));
 		return "filescanner";
 	}
 
 	@PostMapping
-	public String update(Model model, @RequestAttribute Guild guild, @Valid FileScannerConfiguration fileScannerConfiguration,
+	public String update(Model model, @AuthenticationPrincipal DiscordOAuth2User oauth2User,
+			@RequestAttribute Guild guild, @Valid FileScannerConfiguration fileScannerConfiguration,
 			BindingResult bindingResult, final HttpServletResponse res) {
-		System.out.println(fileScannerConfiguration);
 		if (bindingResult.hasErrors()) {
 			res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			return "filescanner";
 		}
-		
+
+		System.out.println(fileScannerConfiguration);
 		fileScannerConfiguration.setGuild(guild.getIdLong());
 		model.addAttribute("fileScannerConfiguration", database.save(fileScannerConfiguration));
+
+		util.logConfigurationChange(oauth2User, guild, "filescanner");
 		return "filescanner";
 	}
 }
